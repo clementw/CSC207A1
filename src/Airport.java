5018bb91ae2d8b078323bb620446669c72ab8137
clementw <clement.wsz@outlook.com>
2017-09-21 21:57:47 -0400

    Skeleton (almost) ready, except Flight.getAirports()

---
+++ src/Airport.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Airport {
+    private String name;
+    private ArrayList flights;
+
+    public Airport(String name) {
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public String toString(Flight f) {
+        return null;
+    }
+
+    public boolean equals(Flight f) {
+        return false;
+    }
+
+    public boolean wasVisitedBy(Flight f) {
+        return false;
+    }
+
+    public boolean onSameFlight(Airport f) {
+        return false;
+    }
+
+    public boolean addFlight(Flight f) {
+        return false;
+    }
+
+
+
+
+
+}
+
+
---
+++ src/Flight.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Flight {
+    private String name;
+    private ArrayList airports;
+    private String date;
+
+    public Flight(String name, String date) {
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public Airport[] getAirports() {
+        return null;
+    }
+
+    public String getDate() {
+        return null;
+    }
+
+    public String toString(Airport f) {
+        return null;
+    }
+
+    public boolean equals(Airport f) {
+        return false;
+    }
+
+    public boolean wasVisitedBy(Airport f) {
+        return false;
+    }
+
+    public boolean onSameFlight(Airport f) {
+        return false;
+    }
+
+    public boolean addAirport(Airport f) {
+        return false;
+    }
+}
